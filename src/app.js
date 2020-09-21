36c441f7caf0169b9a59815ffde208cdf2d7e9b3
Anthony Kowalski <anthonyrkowalski@gmail.com>
2020-09-21 15:48:54 -0700

    Update static route for serving files

--- src/app.js
+++ src/app.js
@@ -15,10 +15,11 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
-// app.set('views', '/views')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('src'));
 
 app.use(cors());
 
