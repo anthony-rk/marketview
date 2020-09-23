fefb1c6198011c1b2d51bbdf6ef8684f4157f738
Anthony Kowalski <anthonyrkowalski@gmail.com>
2020-09-23 15:05:34 -0700

    Update user signup info

--- src/models/user.js
+++ src/models/user.js
@@ -5,6 +5,9 @@ var Schema = mongoose.Schema;
 var UserSchema = new Schema(
   {
     username: {type: String, required: true, maxlength: 100},
+    password: { type: String, required: true },
+    email: {type: String, required: true, maxlength: 100},
+
   }
 );
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,35 @@
-const express = require('express');
-const router = express.Router();
+var express = require('express');
+var router = express.Router();
+
+// const user_controller = require('../controller/userController');
+var userController = require('../controller/userController');
 
 router.get('/', (req, res) => {
-    // return res.send("Hello there!");
     res.render('index');
+});
+
+router.get('/about', (req, res) => {
+    res.render('about');
+});
+
+router.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+router.post('/signup', userController.user_create_post);
+
+// Test for POST
+// router.post('/signup', (req, res) => {
+//     res.send("hello there!");
+// });
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+// Test for POST
+router.post('/login', (req, res) => {
+    res.send("hello there login page!");
 });
 
 module.exports = router;
