b654ed3841838971ba2b150a280bdd5a2f12a1fe
Anthony Kowalski <anthonyrkowalski@gmail.com>
2020-09-23 15:04:49 -0700

    Update app.use() requirements

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const cors = require('cors');
 const path = require('path');
 const partials = require('express-partials');
 const mongoose = require('mongoose');
+const session = require("express-session");
+
 
 // Setup MongoDB connection
 const mongoDb = process.env.MONGODB_URI;
@@ -16,22 +18,18 @@ const indexRouter = require('./routes/index');
 
 const app = express();
 
-
-app.set('view engine', 'ejs');
 app.use(partials());
 
 app.set('views', path.join(__dirname, '/views'));
-
+app.set('view engine', 'ejs');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static('src'));
 
 app.use(cors());
 
-// app.use('/session', routes.session);
-// app.use('/users', routes.user);
 app.use('/', indexRouter);
 
 app.listen(process.env.PORT || 3000, () => 
