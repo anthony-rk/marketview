412d3b1d3f214d9cddaa3355ca4ed195bb948692
Anthony Kowalski <anthonyrkowalski@gmail.com>
2020-10-14 17:43:01 -0700

    Update for Heroku deployment

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -179,8 +179,8 @@ exports.get_stock_price_history_data = function(req, res) {
         const stockResponse = await fetchStockDetails(siteForStockData, requestOptions);
         const stockCompanyResponse = await fetchCompanyDetails(siteForCompanyData, requestOptions);
 
-        console.log(stockResponse);
-        console.log(stockCompanyResponse);
+        // console.log(stockResponse);
+        // console.log(stockCompanyResponse);
 
       res.render('index', {
           stockResponse: stockResponse,
